99a37b4ff983ba60b3a5bd3e519d069603da180b
Gaurav Mishra <gaurav.m@gojek.com>
2023-08-28 16:20:08 +0530

    Update execute permission for gradlew command.

--- shared/src/iosMain/kotlin/com/greenbot/movieskmm/platformModule.kt
+++ shared/src/iosMain/kotlin/com/greenbot/movieskmm/platformModule.kt
@@ -6,9 +6,12 @@ import io.ktor.client.engine.darwin.Darwin
 import org.koin.dsl.module
 
 actual fun platformModule() = module {
+    // local db
     single<SqlDriver> {
         val driver = NativeSqliteDriver(AppDatabase.Schema, "test.db")
         driver
     }
+
+    // network client
     single { Darwin.create() }
 }
